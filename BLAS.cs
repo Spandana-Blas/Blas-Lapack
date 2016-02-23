991763223d1a7b2b62a3c6a397ecac2668d66d76
Spandana-Blas <spandana.vallabhaneni@uconn.edu>
2016-02-23 03:24:13 -0500

    Update BLAS.cs

--- BLAS.cs
+++ BLAS.cs
@@ -128,6 +128,118 @@ namespace Demo_Lapack
             }
             return G;
         }
+        public Matrix Blas4(String mat1, String mat2)
+        {
+            Matrix L = new Matrix();
+            Matrix U = new Matrix();
+            Matrix Y = new Matrix();
+            Matrix X = new Matrix();
+            String toPass = null;
+            String toPass1 = null;
+
+            //String toPass1 = null;
+            L.data = new double[10, 10];
+            U.data = new double[10, 10];
+            Y.data = new double[10, 10];
+            X.data = new double[10, 10];
+            var A = JsonConvert.DeserializeObject<Matrix>(mat1);
+            var B = JsonConvert.DeserializeObject<Matrix>(mat2);
+            int P = A.size[0];
+            int Q = A.size[1];
+            int R = B.size[0];
+            
+            int S = B.size[1];
+            for (int a = 0; a < P; a++)
+            {
+
+                for (int b = 0; b < P; b++)
+                {
+                    L.data[a, b] = 0;
+                    U.data[a, b] = 0;
+                    Y.data[a, b] = 0;
+                    X.data[a, b] = 0;
+                }
+               
+            }
+
+            for (int a = 0; a < P; a++)
+            {
+                for (int b = 0; b < P; b++)
+                {
+                    if (a <= b)
+                    {
+                        U.data[a, b] = A.data[a, b];
+                        for (int c = 0; c <= a - 1; c++)
+                        {
+                            U.data[a, b] -= (L.data[a, c] * U.data[c, b]);
+                            U.data[a, b] = Math.Round(U.data[a, b], 2);
+                        }
+                        if (a == b)
+                            L.data[a, b] = 1;
+                        else
+                            L.data[a, b] = 0;
+
+                    }
+                    else
+                    {
+                        L.data[a, b] = A.data[a, b];
+                        for (int c = 0; c <= b - 1; c++)
+                        {
+                            L.data[a, b] -= (L.data[a, c] * U.data[c, b]);
+                            L.data[a, b] = Math.Round(L.data[a, b], 2);
+                        }
+
+                        L.data[a, b] /= U.data[b, b];
+                        L.data[a, b] = Math.Round(L.data[a, b], 2);
+                        U.data[a, b] = 0;
+                    }
+                }
+            }
+            //Now we solve Y where LY = B
+            for (int a = 0; a < P; a++)
+            {
+                Y.data[a, 0] = B.data[a, 0];
+                for (int b = 0; b < a; b++)
+                {
+                    Y.data[a, 0] = Y.data[a, 0] - (L.data[a, b] * Y.data[b, 0]);
+                    Y.data[a, 0] = Math.Round(Y.data[a, 0], 2);
+                }
+            }
+            int h = 0;
+            for (int a = P - 1; a >= 0; a--)
+            {
+                int b;
+                h = h + 1;
+                X.data[a, 0] = Y.data[a, 0];
+                for (b = a + 1; b < P; b++)
+                {
+                    X.data[a, 0] -= (U.data[a, b] * X.data[b, 0]);
+                    X.data[a, 0] = Math.Round(X.data[a, 0], 2);
+                }
+                X.data[a, 0] /= U.data[a, a];
+                X.data[a, 0] = Math.Round(X.data[a, 0], 2);
+                String strX = X.data[a, 0].ToString();
+                //toPass += strX;
+                if (h != 2)
+                {
+                    toPass += '[' + strX + ']' + ',';
+                }
+                else
+                {
+                    toPass += '[' + strX + ']';
+                }
+
+            }
+
+            toPass1 = "[" + toPass +"]";
+            //String strH = "{\"data\": ["+ toPass+"]}";
+            String strH = "{\"data\": ["+toPass1+"]}";
+            Clients.All.DisplayBlas4(strH);
+
+            return L;
+
+
+        }
         public class Matrix
         {
             public string matrixType { get; set; }
